7647e139998b4728ba1d6e4f5d16c2154c93cdd0
Julijinimo <ownflame@gmail.com>
2022-06-27 23:37:31 +0300

    uploadinu

---
+++ Intro1/main.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    alert('Buttonas buvo paspaustas!')
+});
+
+console.log('Mano informacine zinute');
+
+
+
